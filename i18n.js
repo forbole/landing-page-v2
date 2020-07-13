1aeb4a9fceae1eb6846a85c8f414aa2ef62cc63e
Wingman <ryuash123@gmail.com>
2020-07-13 13:36:38 +0800

    add: i18n

--- i18n.js
+++ i18n.js
@@ -3,9 +3,5 @@ const NextI18Next = require("next-i18next").default;
 module.exports = new NextI18Next({
   defaultLanguage: "en",
   otherLanguages: [],
-});
-
-module.exports = new NextI18Next({
-  otherLanguages: [],
-  localeSubpaths: localeSubpathVariations[localeSubpaths],
+  localeSubpaths: {},
 });
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const next = require("next");
 const nextI18NextMiddleware = require("next-i18next/middleware").default;
 
 const nextI18next = require("./i18n");
-
 const port = process.env.PORT || 3000;
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handle = app.getRequestHandler();
@@ -15,7 +14,7 @@ const handle = app.getRequestHandler();
   await nextI18next.initPromise;
   server.use(nextI18NextMiddleware(nextI18next));
 
-  server.get("*", (req: any, res: any) => handle(req, res));
+  server.get("*", (req, res) => handle(req, res));
 
   await server.listen(port);
   console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
