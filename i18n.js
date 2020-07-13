baa09adc3a6c6918bd0fdfe52fcd27579b4bf761
Wingman <ryuash123@gmail.com>
2020-07-13 12:37:26 +0800

    wip: adding i18n

---
+++ i18n.js
@@ -0,0 +1,11 @@
+const NextI18Next = require("next-i18next").default;
+
+module.exports = new NextI18Next({
+  defaultLanguage: "en",
+  otherLanguages: [],
+});
+
+module.exports = new NextI18Next({
+  otherLanguages: [],
+  localeSubpaths: localeSubpathVariations[localeSubpaths],
+});
