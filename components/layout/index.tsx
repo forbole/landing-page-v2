907f21762be899e7ce804fcddb26f7ddcda5a0b8
Wingman <ryuash123@gmail.com>
2020-07-14 11:40:24 +0800

    update tsconfig paths

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from "react";
-import Link from "next/link";
 import Head from "next/head";
+import NavBar from "@components/navbar";
 import "./styles.scss";
-import "./indexdef.scss";
+import "./global.scss";
 
 type Props = {
   children?: ReactNode;
@@ -16,68 +16,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-
-    <header className="main-header">
-      <div className="logo">
-        <Link href="/">
-          <a>
-            <img
-              src="/forbole_logo/forbole_logo_red.svg"
-              alt="Forbole Logo"
-            ></img>
-          </a>
-        </Link>
-      </div>
-      <input type="checkbox" className="menu-btn" id="menu-btn" />
-      <label htmlFor="menu-btn" className="menu-icon">
-        <span className="menu-icon_line"></span>
-      </label>
-      <ul className="nav-links">
-        <li className="nav-link">
-          <Link href="stake-now">
-            <a>Stake Now</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="staking">
-            <a>Staking</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="big-dipper">
-            <a>Big Dipper</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="desmos">
-            <a>Desmos</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="blog">
-            <a>Blog</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li className="nav-link">
-          <Link href="contact">
-            <a>Contact</a>
-          </Link>
-        </li>
-        <li className="nav-link dropdown">
-          <Link href="language">
-            <ul className="dropdown-menu">
-              <li className="dropdown-item">ENG</li>
-              <li className="dropdown-item">CHN</li>
-            </ul>
-          </Link>
-        </li>
-      </ul>
-    </header>
+    <NavBar />
     {children}
     <footer>
       <hr />
