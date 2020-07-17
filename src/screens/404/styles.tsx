c083375255f2213353cf262616e2240a2d8003ae
Wingman <ryuash123@gmail.com>
2020-07-17 17:02:44 +0800

    add: desktop 404 without layout

--- src/screens/404/styles.tsx
+++ src/screens/404/styles.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const NotFoundCSS = styled.div`
   background-image: linear-gradient(
       0deg,
-      rgba(0, 0, 0, 0.7),
-      rgba(0, 0, 0, 0.7)
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.9)
     ),
     url("forbole_background/404_background.jpeg");
   background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: center;
+  background-position: center bottom;
+  background-size: cover;
   min-height: 100vh;
   display: flex;
   align-items: center;
@@ -33,5 +33,9 @@ export const NotFoundCSS = styled.div`
   p {
     color: white;
     text-align: center;
+  }
+
+  img {
+    max-width: 300px;
   }
 `;
