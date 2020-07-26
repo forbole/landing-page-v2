fe0fe36d4d7c34ac27249ab3782a392668736615
Wingman <ryuash123@gmail.com>
2020-07-26 21:08:34 +0800

    add seo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { NavBar, Footer } from "@components";
 import { LayoutCSS } from "./styles";
 
@@ -9,11 +8,19 @@ type Props = {
   title?: string;
 };
 const Layout = ({ children, title = "This is the default title" }: Props) => {
-  const router = useRouter();
   return (
     <LayoutCSS>
       <Head>
         <title>{title}</title>
+        {/* wingman edit later */}
+        <meta name="og:type" content="website" />
+        <meta name="description" content="" />
+        <meta name="og:title" content="Forbole" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="og:url" content={process.env.URL} />
+        <meta name="og:description" content="" />
+        <meta name="og:image" content="" />
       </Head>
       <NavBar />
       {children}
--- src/components/layout/styles.tsx
+++ src/components/layout/styles.tsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 
 export const LayoutCSS = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+
 `;
