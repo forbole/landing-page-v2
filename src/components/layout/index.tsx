5a149e48627555dbcfafb0d308a3db0b4131aa9c
Wingman <ryuash123@gmail.com>
2020-07-27 10:24:52 +0800

    cleanup

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { NavBar, Footer } from "@components";
 import { MainContentCSS } from "./styles";
 
@@ -8,23 +9,26 @@ type Props = {
   title?: string;
 };
 const Layout = ({ children, title = "This is the default title" }: Props) => {
+  const router = useRouter();
   return (
     <>
       <Head>
         <title>{title}</title>
-        {/*  wingman edit later */}
+        {/* refactor this in to document later */}
         <meta name="og:type" content="website" />
         <meta name="description" content="" />
         <meta name="og:title" content="Forbole" />
         <meta name="description" content="" />
         <meta name="keywords" content="" />
-        <meta name="og:url" content={process.env.URL} />
+        <meta name="og:url" content={`${process.env.URL}${router.pathname}`} />
         <meta name="og:description" content="" />
         <meta name="og:image" content="" />
       </Head>
-      <NavBar />
-      <MainContentCSS>{children}</MainContentCSS>
-      <Footer />
+      <body>
+        <NavBar />
+        <MainContentCSS>{children}</MainContentCSS>
+        <Footer />
+      </body>
     </>
   );
 };
