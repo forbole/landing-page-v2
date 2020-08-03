3ebf3ee401b3ae37ed4c2eacf2753677ae2520ee
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-08-03 14:32:49 +0800

    public updates

--- src/screens/cosmos_hub/components/cosmos_header/index.tsx
+++ src/screens/cosmos_hub/components/cosmos_header/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useTranslation } from "i18n";
 import { CosmosHeaderCSS } from "./styles";
 
 function CosmosHeader() {
-  return <CosmosHeaderCSS></CosmosHeaderCSS>;
+  const { t } = useTranslation("cosmos_hub");
+  return (
+    <CosmosHeaderCSS>
+      <img src="images/icons/cosmos_hub.png" alt="cosmos-hub"></img>
+      <p>{t("cosmosHub")}</p>
+    </CosmosHeaderCSS>
+  );
 }
 
 export default CosmosHeader;
