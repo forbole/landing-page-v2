a153fbe70ee8b4adf68ce9b96834554c1c82bf0e
Wingman J. Lee <42913823+ryuash@users.noreply.github.com>
2020-08-04 16:01:22 +0800

    Fix/flex (#73)
    * fix: flex
    
    * fix: flex
    
    * add flex

--- src/screens/cosmos_hub/components/cosmos_content/styles.tsx
+++ src/screens/cosmos_hub/components/cosmos_content/styles.tsx
@@ -2,27 +2,38 @@ import styled from "styled-components";
 import { media, mixins } from "@styles";
 
 export const CosmosContentCSS = styled.div`
-  padding: 1.5rem 1rem;
+  ${mixins.mobileLastContainerPadding}
+  flex: 1;
+
   p {
     font-size: 1rem;
     margin-bottom: 2em;
   }
-  ${mixins.mobileLastContainerPadding}
+
   ${media.tablet`
-  padding: 3rem;
-p {
-    font-size: 2rem;
-}`}
+    padding: 3rem;
+    p {
+        font-size: 2rem;
+    }
+  `}
+
   ${media.bigDesktop`
-  ${mixins.desktopMaxWidth}
-  width: 100%;
-  height: 100%;
-  padding: 3rem 0;
-  ${mixins.desktopBottomPadding}
-p {
-    justify-content: flex-start;
+    ${mixins.desktopBottomPadding}
+    display: flex;
+    justify-content: center;
     width: 100%;
-    font-weight: 100;
-    font-size: 1rem;
-  }`}
+    padding: 3rem 0;
+
+    .wrapper {
+      ${mixins.desktopMaxWidth}
+      width: 100%;
+    }
+
+    p {
+        justify-content: flex-start;
+        width: 100%;
+        font-weight: 100;
+        font-size: 1rem;
+      }
+  `}
 `;
--- src/screens/cosmos_hub/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const CosmosHubCSS = styled.div`
-  .wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
