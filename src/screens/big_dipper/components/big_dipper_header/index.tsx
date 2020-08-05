3695fbba27d1eb46a27d8f77d1d3b46dfd1f1a56
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-08-05 23:34:37 +0800

    mobile and desktop view updates

--- src/screens/big_dipper/components/big_dipper_header/index.tsx
+++ src/screens/big_dipper/components/big_dipper_header/index.tsx
@@ -2,23 +2,27 @@ import React from "react";
 import { useTranslation } from "i18n";
 import { BDHeaderCSS } from "./styles";
 import { socialMedias } from "./config";
+import { Go } from "@icons";
 
 export const BDHeader = () => {
   const { t } = useTranslation("big_dipper");
   return (
     <BDHeaderCSS>
-      <h1>{t("title")}</h1>
-      <h2>{t("yourCosmosExplorer")}</h2>
-      <h3>{t("visitWebsite")}</h3>
-      <span>
-        <div className="socialMedia">
-          {socialMedias.map((x, i) => (
-            <a href={x.url} key={i}>
-              <x.component />
-            </a>
-          ))}
-        </div>
-      </span>
+      <div className="wrapper">
+        <h1>{t("title")}</h1>
+        <h2>{t("yourCosmosExplorer")}</h2>
+        <span>
+          <a href="https://cosmos.bigdipper.live/">{t("visitWebsite")}</a>
+          <Go />
+        </span>
+      </div>
+      <div className="socialMedia">
+        {socialMedias.map((x, i) => (
+          <a href={x.url} key={i}>
+            <x.component />
+          </a>
+        ))}
+      </div>
     </BDHeaderCSS>
   );
 };
--- src/screens/big_dipper/components/big_dipper_header/styles.tsx
+++ src/screens/big_dipper/components/big_dipper_header/styles.tsx
@@ -1,6 +1,35 @@
 import styled from "styled-components";
-import { mixins } from "@styles";
+import { mixins, theme } from "@styles";
+
+const { colors } = theme;
 
 export const BDHeaderCSS = styled.div`
-  ${mixins.mobileLastContainerPadding}
+  ${mixins.mobilePadding}
+  padding-top: 5rem;
+  background-color: ${colors.red};
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  a {
+    color: ${colors.white};
+    padding-right: 0.5rem;
+  }
+  .socialMedia > a {
+    padding-right: 1rem;
+  }
+  h1,
+  h2,
+  span {
+    color: ${colors.white};
+    margin-bottom: 0.5rem;
+    font-weight: 100;
+  }
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  svg > path {
+    fill: white;
+  }
 `;
