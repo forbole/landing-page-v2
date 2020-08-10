d32a34ccf3876c02a16c1731f3387de11582d4b5
Wingman <ryuash123@gmail.com>
2020-08-10 17:55:11 +0800

    fix: ready on console

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,9 @@ const port = process.env.PORT || 3000;
     });
     server.listen(port, (err?: any) => {
       if (err) throw err;
-      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+      console.log(
+        `> Ready on http://localhost:${port} - env ${process.env.NODE_ENV}`
+      );
     });
   } catch (e) {
     console.error(e);
