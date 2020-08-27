58474259e0a87e25b5f725a7a10de350df7380d4
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-08-27 11:26:27 +0800

    update: mobile view

--- src/screens/home/components/hiring/styles.tsx
+++ src/screens/home/components/hiring/styles.tsx
@@ -12,11 +12,10 @@ export const HiringHeaderCSS = styled.div`
   //   background: linear-gradient(0deg, rgb(0, 0, 0, 0.4), rgb(0, 0, 0, 0.1)),
   //     url("static/images/assets/joinUs.png/");
   min-height: 30vh;
-  //   margin-top: 2rem;
   ${mixins.mobileVerticalPadding}
+  padding-top: 3.5rem;
   height: 20rem;
   width: 100%;
-  //   object-fit: cover;
 
   img {
     zoom: 0.44;
@@ -31,4 +30,24 @@ export const HiringHeaderCSS = styled.div`
   }
 `;
 
-export const ContentWrapperCSS = styled.div``;
+export const ContentWrapperCSS = styled.div`
+  ${mixins.mobilePadding}
+  ${mixins.mobileBottomPadding}
+  padding-top: 1.5rem;
+  h1 {
+    font-weight: 100;
+    margin-bottom: 0.5rem;
+  }
+  h2 {
+    font-weight: 100;
+    font-size: 0.8rem;
+  }
+  h3 {
+    font-weight: 100;
+    font-size: 1rem;
+    margin-bottom: 3rem;
+  }
+  .ui.red.button {
+    background-color: rgba(255, 80, 80, 1);
+  }
+`;
