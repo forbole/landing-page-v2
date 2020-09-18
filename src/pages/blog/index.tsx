bfd4d10d97cd0f585c6e09ef81cfb740ec2e45db
Wingman <ryuash123@gmail.com>
2020-09-18 13:48:38 +0800

    setup query pages

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,18 @@
 import Blog from "@screens/blog";
+import { useRouter } from "next/router";
 import { getPosts } from "@api/posts";
 import { Post } from "@models";
 function BlogPage(props: any) {
   return <Blog {...props} />;
 }
 
-BlogPage.getInitialProps = async () => {
-  const posts = await getPosts({});
+BlogPage.getInitialProps = async ({ query }) => {
+  console.log(query, "router q");
+  const fetchQuery: any = {};
+  if (query.page) {
+    fetchQuery.page = query.page;
+  }
+  const posts = await getPosts(fetchQuery);
   const formattedPosts = posts.map((post) => Post.fromJson(post));
 
   return { posts: formattedPosts, meta: posts.meta };
