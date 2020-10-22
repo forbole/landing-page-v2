816b5423e09a505b42448c0c364584c5e3896acf
Wingman <ryuash123@gmail.com>
2020-10-22 14:34:18 +0800

    fix: footer

--- src/screens/big_dipper/styles.tsx
+++ src/screens/big_dipper/styles.tsx
@@ -4,11 +4,17 @@ const { colors } = theme;
 export const BigDipperCSS = styled.span`
   a.active {
     color: white;
+    div {
+      color: white;
+    }
   }
 
   .displayBackground {
     a.active {
       color: ${() => colors.forboleRed};
+      div {
+        color: ${() => colors.forboleRed};
+      }
     }
   }
 `;
