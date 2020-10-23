7b730cc97aff1b3ed9e2c9ac4ccad34ece4f44fd
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-10-23 15:41:37 +0800

    mailgun stuff

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,9 @@
 /* eslint-disable */
-import 'dotenv-defaults/config';
+import "dotenv-defaults/config";
 import express, { Request, Response } from "express";
 import next from "next";
 import cors from "cors";
+import nodemailer from "nodemailer";
 
 const nextI18NextMiddleware = require("next-i18next/middleware").default;
 const nextI18next = require("../i18n");
@@ -11,6 +12,15 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT;
 const url = process.env.URL;
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailgun.org",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_GUN_USER,
+    pass: process.env.MAIL_GUN_PW,
+  },
+});
 
 (async () => {
   try {
@@ -20,6 +30,13 @@ const url = process.env.URL;
     await nextI18next.initPromise;
     server.use(cors());
     server.use(nextI18NextMiddleware(nextI18next));
+    server.use(express.json());
+
+    server.post("/api/contact", (req: Request, res: Response) => {
+      console.log(req.body);
+      console.log("hiiiii");
+      transporter.sendMail(req.body);
+    });
 
     server.all("*", (req: Request, res: Response) => {
       return handle(req, res);
@@ -27,10 +44,12 @@ const url = process.env.URL;
     server.listen(port, (err?: any) => {
       if (err) throw err;
       console.log(`> Ready on ${url}:${port}`);
-      console.log(`> ENV:  ${process.env.ENV}`)
+      console.log(`> ENV:  ${process.env.ENV}`);
     });
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 })();
+
+export default transporter;
--- src/screens/contact/components/contact_message/hooks.tsx
+++ src/screens/contact/components/contact_message/hooks.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import validator from "validator";
+import nodemailer from "nodemailer";
+import transporter from "server/index";
+import axios from "axios";
 
 const useContactForm = () => {
   const [inputs, setInputs] = useState({ name: "", message: "", email: "" });
@@ -20,6 +23,16 @@ const useContactForm = () => {
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
+      axios
+        .post("/contact", {
+          from: inputs.email,
+          to: "hi@forbole.com",
+          subject: "Hi Forbole!",
+          text: inputs.message,
+          html: `<p>${inputs.message}</p>`,
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
     }
   };
   const handleInputChange = (event) => {
