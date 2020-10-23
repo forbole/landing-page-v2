ada5e251f8d770190f77b423e8846761ef1a558c
Wingman <ryuash123@gmail.com>
2020-10-23 15:31:33 +0800

    remove data

--- src/screens/network/components/network_header/index.tsx
+++ src/screens/network/components/network_header/index.tsx
@@ -5,7 +5,7 @@ import { getNetworkInfo } from "@utils/network-info";
 function NetworkHeader(props: any) {
   const { networkKey = "" } = props;
   const data = getNetworkInfo(networkKey);
-  console.log(data, "my data");
+
   return (
     <NetworkHeaderCSS>
       <div className="wrap">
