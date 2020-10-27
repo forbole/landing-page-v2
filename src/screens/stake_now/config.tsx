e31f4ca0e0394212ea08bb6b520c10b97297976e
Wingman <ryuash123@gmail.com>
2020-10-27 10:55:21 +0800

    wip

--- src/screens/stake_now/config.tsx
+++ src/screens/stake_now/config.tsx
@@ -1,68 +1,31 @@
-export const networkData = [
-  {
-    image: "static/images/icons/cosmos-hub.png",
-    key: "cosmosHub",
-    name: "Cosmos Hub",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/polkadot.png",
-    key: "polkadot",
-    name: "Polkadot",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/kava.png",
-    key: "kava",
-    name: "Kava",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/desmos.png",
-    key: "desmos",
-    name: "Desmos",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/terra.png",
-    key: "terra",
-    name: "Terra",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/iris.png",
-    key: "irisnet",
-    name: "IRISnet",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/likecoin.png",
-    key: "likecoin",
-    name: "LikeCoin",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/iris.png",
-    key: "cyberCongress",
-    name: "Cyber Congress",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/regen-network.png",
-    key: "regenNetwork",
-    name: "Regen Network",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/sentinel.png",
-    key: "sentinel",
-    name: "Sentinel",
-    amount: 1234,
-  },
-  {
-    image: "static/images/icons/e-money.png",
-    key: "eMoney",
-    name: "e-Money",
-    amount: 1234,
-  },
-];
+export const networkKeys = [
+  "cosmos",
+  "polkadot",
+  "kava",
+  "terra-money",
+  // "iris",
+  // "desmos",
+  "likecoin",
+  // "cyberd",
+  "e-money",
+  // "regen-network",
+  "band-protocol",
+  "akash",
+  "iov",
+  "kusama",
+].sort();
+
+// we should only calculate the ones we are staking..?
+// export const calculatorKeys = [
+//   "cosmos",
+//   "polkadot",
+//   "kava",
+//   "desmos",
+//   "terra-money",
+//   "iris",
+//   "likecoin",
+//   "regen-network",
+//   "sentinel",
+//   "e-money",
+//   // "cyberd",
+// ].sort();
