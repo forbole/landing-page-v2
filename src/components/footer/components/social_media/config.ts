95cb6c93ca5426f7a900b74da13d5e10cb7fc906
Wingman <ryuash123@gmail.com>
2020-10-28 14:13:42 +0800

    add: subscribed placeholder

---
+++ src/components/footer/components/social_media/config.ts
@@ -0,0 +1,29 @@
+import { Telegram, Facebook, Medium, Twitter, Github } from "@icons";
+
+export const socialMediaInfo = [
+  {
+    key: "telegram",
+    url: "https://t.me/forbole",
+    component: Telegram,
+  },
+  {
+    key: "facebook",
+    url: "https://www.facebook.com/Forbole/",
+    component: Facebook,
+  },
+  {
+    key: "medium",
+    url: "https://medium.com/forbole",
+    component: Medium,
+  },
+  {
+    key: "twitter",
+    url: "https://twitter.com/forbole",
+    component: Twitter,
+  },
+  {
+    key: "github",
+    url: "https://github.com/forbole",
+    component: Github,
+  },
+];
--- src/components/footer/components/social_media/index.tsx
+++ src/components/footer/components/social_media/index.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { SocialMediaCSS } from "./styles";
-import { Telegram, Facebook, Medium, Twitter, Github } from "../../../icons";
+import { socialMediaInfo } from "./config";
 
 const SocialMedia = () => {
   return (
     <SocialMediaCSS>
-      <a href="#">
-        <Telegram />
-      </a>
-      <a href="#">
-        <Facebook />
-      </a>
-      <a href="#">
-        <Medium />
-      </a>
-      <a href="#">
-        <Twitter />
-      </a>
-      <a href="#">
-        <Github />
-      </a>
+      {socialMediaInfo.map((x) => {
+        return (
+          <a key={x.key} href={x.url} target="_blank" rel="noreferrer">
+            <x.component />
+          </a>
+        );
+      })}
     </SocialMediaCSS>
   );
 };
