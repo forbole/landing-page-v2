9091e3037f3df05b587e4fda59f8e529658e5645
Wingman <ryuash123@gmail.com>
2020-10-28 15:17:09 +0800

    update:blogs

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -12,13 +12,26 @@ BlogPage.getInitialProps = async ({ query }) => {
     fetchQuery.page = query.page;
   }
 
-  const [tags, posts] = await Promise.all([getTags(), getPosts(fetchQuery)]);
+  const [tags, posts, sidePosts] = await Promise.all([
+    getTags(),
+    getPosts(fetchQuery),
+    getPosts({
+      limit: 10,
+    }),
+  ]);
+
   const formattedPosts = posts.map((post) => Post.fromJson(post, {}));
+  const formattedSidePosts = sidePosts.map((post) => Post.fromJson(post, {}));
   const formattedTags = removeInternalTags(tags).map((tag) =>
     Tag.fromJson(tag)
   );
 
-  return { posts: formattedPosts, meta: posts.meta, tags: formattedTags };
+  return {
+    posts: formattedPosts,
+    meta: posts.meta,
+    tags: formattedTags,
+    sidePosts: formattedSidePosts,
+  };
 };
 
 export default BlogPage;
