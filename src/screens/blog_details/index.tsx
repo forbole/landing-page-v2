e3990808ba4db8a222ae5181c5405904fc25790d
Rachel Ho <rachelhoyuenyi@gmail.com>
2020-11-19 13:46:38 +0800

    schema added

--- src/screens/blog_details/index.tsx
+++ src/screens/blog_details/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import DOMPurify from "isomorphic-dompurify";
 import { Layout, Tags } from "@components";
 import { theme } from "@styles";
@@ -14,7 +15,70 @@ import { Author, SocialMedia } from "./components";
 const { colors } = theme;
 
 const BlogDetails = ({ post, raw }: any) => {
-  const { tags, excerpt, featureImage } = post;
+  const url = process.env.NEXT_PUBLIC_URL;
+  const {
+    title,
+    blurb,
+    date,
+    modified,
+    slug,
+    commentCount,
+    author,
+    ratingCount,
+    ratingAverage,
+    citations,
+    tags,
+    excerpt,
+    featureImage,
+  } = post;
+  console.log(post);
+  const published = new Date(date);
+  const copyrightYear = published.getFullYear();
+
+  let mediaDetails, sourceUrl;
+
+  if (featureImage) {
+    sourceUrl = featureImage.sourceUrl;
+  }
+
+  // const citationsList = citations.map((citation, i) => {
+  //   return `{ "@type": "CreativeWork", "citation": ${JSON.stringify(
+  //     citation
+  //   )} }${i === citations.length - 1 ? "" : ","}\n`;
+  // });
+  // const citationsText = citationsList.join("");
+
+  const org = `{ "@id": "${slug}#organization", "type": "Organization", "name":"${title}", "logo": {
+    "@type": "ImageObject",
+    "name": "${title} Logo",
+    "width": "230",
+    "height": "67",
+    "url": "${url}images/logo.png"
+} }`;
+
+  const jsonData = `{
+  "@context":"https://schema.org/",
+  "@type":"Article",
+  "name":"${title}",
+  "about": "${blurb}",
+  "author": { "@type": "Person", "@id": "${url}author/${author.name}", "name": "author.name" },
+  "commentCount": commentCount,
+  "copyrightHolder": { "@id": "${url}#organization" },
+  "copyrightYear": copyrightYear,
+  "datePublished": "${date}",
+  "dateModified": modified,
+  "description": "${blurb}",
+  "discussionUrl": "${url}articles/${slug}#comments",
+  "editor": { "@id": "${url}author/${author.slug}#author" },
+  "headline": "${title}",
+  "inLanguage": "English",
+  "mainEntityOfPage": "${url}articles/${slug}",
+  "publisher": { "@id": "${url}#organization" },
+  "sourceOrganization": ${org},
+  "url": "${url}articles/${slug}"
+    }}`;
+
+  // console.log(org);
   const sanitize = DOMPurify.sanitize;
   return (
     <Layout
@@ -26,6 +90,12 @@ const BlogDetails = ({ post, raw }: any) => {
       image={featureImage}
       keywords={tags.map((x) => x.name ?? "")}
     >
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonData) }}
+        />
+      </Head>
       <BlogDetailsCSS>
         <MaxWidthContainerCSS>
           <ContentCSS>
