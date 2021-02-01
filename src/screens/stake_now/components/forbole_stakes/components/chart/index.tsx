c252b5d18a0903b1c97fed4dec80a03d2d38c9b7
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-02-01 14:29:00 +0800

    donut chart mouseOver

--- src/screens/stake_now/components/forbole_stakes/components/chart/index.tsx
+++ src/screens/stake_now/components/forbole_stakes/components/chart/index.tsx
@@ -97,11 +97,8 @@ const Chart = (props: any) => {
       }
       labelPosition={0}
       startAngle={0}
-      onClick={(_, index) => {
-        setSelected(index === selected ? undefined : index);
-      }}
       onMouseOver={(_, index) => {
-        setSelected(index);
+        setSelected(index === selected ? undefined : index);
       }}
       onMouseOut={() => {
         setSelected(0);
