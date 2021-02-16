d039fc999b27d86ba0a47a5672da4ce49dbaf959
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-02-16 15:54:42 +0800

    tag page rendering

--- src/screens/tag/index.tsx
+++ src/screens/tag/index.tsx
@@ -25,51 +25,51 @@ const TagTitlePosts = (props: any) => {
   }, [props]);
   const { t } = useTranslation("blog");
   useBlogHook(error, t);
-  return (
-    <>
-      {isLoading ? (
-        <Layout
-          title={t("forbole")}
-          navColor={colors.gray600}
-          mobileNavColor={colors.gray600}
-          description={t("excerpt")}
-          type="article"
-          image={t("forbole")}
-        >
-          <BlogCSS>
-            <MaxWidthContainerCSS>
-              <TagDetailsLoader />
-            </MaxWidthContainerCSS>
-          </BlogCSS>
-        </Layout>
-      ) : (
-        <Layout
-          title={props.post.title}
-          navColor={colors.gray600}
-          mobileNavColor={colors.gray600}
-          description={props.post.excerpt}
-          type="article"
-          image={props.post.featureImage}
-          keywords={tags.map((x) => x.name ?? "")}
-        >
-          <BlogCSS>
-            <MaxWidthContainerCSS>
-              <TagPosts
-                main={props.post[0]}
-                blogs={props.post.slice(1)}
-                meta={meta}
-              />
-              <SideCSS>
-                <TitlePosts posts={sidePosts} />
-                <Tags tags={tags} />
-                <Twitter />
-              </SideCSS>
-            </MaxWidthContainerCSS>
-          </BlogCSS>
-        </Layout>
-      )}
-    </>
-  );
+  if (isLoading) {
+    return (
+      <Layout
+        title={t("forbole")}
+        navColor={colors.gray600}
+        mobileNavColor={colors.gray600}
+        description={t("excerpt")}
+        type="article"
+        image={t("forbole")}
+      >
+        <BlogCSS>
+          <MaxWidthContainerCSS>
+            <TagDetailsLoader />
+          </MaxWidthContainerCSS>
+        </BlogCSS>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout
+        title={props.post.title}
+        navColor={colors.gray600}
+        mobileNavColor={colors.gray600}
+        description={props.post.excerpt}
+        type="article"
+        image={props.post.featureImage}
+        keywords={tags.map((x) => x.name ?? "")}
+      >
+        <BlogCSS>
+          <MaxWidthContainerCSS>
+            <TagPosts
+              main={props.post[0]}
+              blogs={props.post.slice(1)}
+              meta={meta}
+            />
+            <SideCSS>
+              <TitlePosts posts={sidePosts} />
+              <Tags tags={tags} />
+              <Twitter />
+            </SideCSS>
+          </MaxWidthContainerCSS>
+        </BlogCSS>
+      </Layout>
+    );
+  }
 };
 
 export default TagTitlePosts;
