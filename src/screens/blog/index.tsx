0df25a269d51272587d831a7447d73309fec365e
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-02-16 21:59:57 +0800

    blog page loader

--- src/screens/blog/index.tsx
+++ src/screens/blog/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "i18n";
-import { Layout, Tags } from "@components";
+import { Layout, Tags, TagDetailsLoader } from "@components";
 import { theme } from "@styles";
 import { BlogPosts, TitlePosts, Twitter } from "./components";
 import { BlogCSS, MaxWidthContainerCSS, SideCSS } from "./styles";
@@ -11,6 +11,12 @@ const Blog = (props: any) => {
   const { posts = [], meta = {}, tags = [], sidePosts = [], error } = props;
   const { t } = useTranslation("blog");
   useBlogHook(error, t);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    if (posts.length > 0 && posts[0].featureImage && sidePosts.length > 0) {
+      setLoading(false);
+    }
+  }, [props]);
   return (
     <Layout
       title={t("title")}
@@ -19,12 +25,20 @@ const Blog = (props: any) => {
     >
       <BlogCSS>
         <MaxWidthContainerCSS>
-          <BlogPosts main={posts[0]} blogs={posts.slice(1)} meta={meta} />
-          <SideCSS>
-            <TitlePosts posts={sidePosts} />
-            <Tags tags={tags} />
-            <Twitter />
-          </SideCSS>
+          {isLoading ? (
+            <>
+              <TagDetailsLoader />
+            </>
+          ) : (
+            <>
+              <BlogPosts main={posts[0]} blogs={posts.slice(1)} meta={meta} />
+              <SideCSS>
+                <TitlePosts posts={sidePosts} />
+                <Tags tags={tags} />
+                <Twitter />
+              </SideCSS>
+            </>
+          )}
         </MaxWidthContainerCSS>
       </BlogCSS>
     </Layout>
