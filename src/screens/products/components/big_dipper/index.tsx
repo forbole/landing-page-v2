e4989ecc768c00c510a45d9a654290d1959022f3
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-03-15 16:22:24 +0800

    nav bar

--- src/screens/products/components/big_dipper/index.tsx
+++ src/screens/products/components/big_dipper/index.tsx
@@ -12,17 +12,9 @@ const BigDipper = () => {
   const { t } = useTranslation("products");
   return (
     <BigDipperCSS>
-      <Layout
-        title={t("title")}
-        description={t("description")}
-        image="/static/images/assets/Facebook-Bigdipper.png"
-        twitterImage="/static/images/assets/Twitter-Bigdipper.png"
-      >
-        {/* <BigDipperHeader /> */}
-        <BigDipperContent />
-        <BlockchainProjects />
-        {/* <DashboardContent /> */}
-      </Layout>
+      <BigDipperContent />
+      <BlockchainProjects />
+      {/* <DashboardContent /> */}
     </BigDipperCSS>
   );
 };
