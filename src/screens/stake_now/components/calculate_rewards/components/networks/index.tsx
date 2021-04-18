07854fe6250b95ca5b8c3be805adee9da2400a76
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-04-19 03:32:47 +0800

    drop down network logic done

--- src/screens/stake_now/components/calculate_rewards/components/networks/index.tsx
+++ src/screens/stake_now/components/calculate_rewards/components/networks/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
+import { Dropdown } from "semantic-ui-react";
 import { useTranslation } from "i18n";
 import { getNetworkInfo } from "@src/utils/network_info";
 import { calculatorKeys } from "./config";
@@ -7,13 +8,70 @@ import { Button, NetworkChoicesCSS } from "./styles";
 import { INetworkProps } from "./interfaces";
 import { ParagraphTitleCSS } from "../../styles";
 
+// const Dropdown = (props: any) => {
+//   console.log(props);
+//   const { options } = props;
+//   console.log(options);
+//   const [selectedOption, setSelectedOption] = useState(options[0].value);
+//   return (
+//     <select
+//       value={selectedOption}
+//       onChange={(e) => setSelectedOption(e.target.value)}
+//     >
+//       {options.map((o) => (
+//         <option key={o.value} value={o.value}>
+//           {o.label}
+//         </option>
+//       ))}
+//     </select>
+//   );
+// };
+
 const Networks = (props: INetworkProps) => {
   const { t } = useTranslation("stake_now");
   const { selectedToken, setSelectedToken } = props;
+
   const networkData = calculatorKeys.map((x) => getNetworkInfo(x));
+  const [selectedOption, setSelectedOption] = useState(networkData[0]);
+  const handleSelect = (e) => {
+    console.log(`target`, e.currentTarget.value);
+    setSelectedToken(e.target.value);
+  };
+  const handleOnChange = (event, data) => {
+    event.persist();
+    //console.log(`hi`, event.target.innerText, data.value[1].text);
+    for (let i = 0; i < networkData.length; i++) {
+      console.log(data.value[i]);
+      if (
+        data.value[i].hasOwnProperty("text") &&
+        event.target.innerText == data.value[i].text
+      ) {
+        setSelectedOption(data.value[i].key);
+        setSelectedToken(data.value[i].key);
+      }
+    }
+
+    if (event.value == "Select All") {
+      console.log(event);
+      console.log(event.target);
+    }
+    //console.log(`selected`, selectedOption);
+  };
+  console.log(`selected`, selectedOption, selectedToken);
+
   return (
     <div>
       <ParagraphTitleCSS>{t("selectNetwork")}</ParagraphTitleCSS>
+      <Dropdown
+        placeholder="Select Network"
+        //value={networkData}
+        fluid
+        multiple
+        selection
+        options={networkData}
+        onChange={handleOnChange}
+        // className={classNames({ active: x.key == selectedToken })}
+      />
       <NetworkChoicesCSS>
         {networkData.map((x) => (
           <Button
--- src/utils/network_info.ts
+++ src/utils/network_info.ts
@@ -8,7 +8,10 @@ const logos = {
   cosmos: {
     image: "/static/images/icons/cosmos-hub.png",
     name: "Cosmos Hub",
+    text: "Cosmos Hub - ATOM",
     key: "cosmos",
+    value: "cosmos",
+    denom: "ATOM",
     bigDipper: "https://cosmos.bigdipper.live/",
     delegate:
       "https://cosmos.bigdipper.live/validators/cosmosvaloper14kn0kk33szpwus9nh8n87fjel8djx0y070ymmj/delegate",
@@ -24,7 +27,9 @@ const logos = {
   iris: {
     image: "/static/images/icons/iris.png",
     name: "IRIS Hub",
+    text: "IRIS Hub - IRIS",
     key: "iris",
+    denom: "IRIS",
     bigDipper: "https://iris.bigdipper.live/",
     delegate:
       "https://iris.bigdipper.live/validators/iva1msqqkd3v0gmullzwm56c4frevyczzxfeczvjru/delegate",
@@ -40,7 +45,9 @@ const logos = {
   ["terra-money"]: {
     image: "/static/images/icons/terra.png",
     name: "Terra Money",
+    text: "Terra Money - LUNA",
     key: "terra-money",
+    denom: "LUNA",
     delegate:
       "https://app.lunie.io/terra/validators/terravaloper1jkqr2vfg4krfd4zwmsf7elfj07cjuzss30ux8g",
     heightSocket: "wss://ws.terra.forbole.com",
@@ -55,7 +62,9 @@ const logos = {
   kava: {
     image: "/static/images/icons/kava.png",
     name: "Kava",
+    text: "Kava - KAVA",
     key: "kava",
+    denom: "KAVA",
     bigDipper: "https://kava.bigdipper.live/",
     delegate:
       "https://kava.bigdipper.live/validators/kavavaloper14kn0kk33szpwus9nh8n87fjel8djx0y02c7me3/delegate",
@@ -77,7 +86,9 @@ const logos = {
   likecoin: {
     image: "/static/images/icons/likecoin.png",
     name: "LikeCoin",
+    text: "LikeCoin - LIKE",
     key: "likecoin",
+    denom: "LIKE",
     bigDipper: "http://likecoin.bigdipper.live/",
     delegate:
       "https://likecoin.bigdipper.live/validator/cosmosvaloper1v8njts96gl5eqstnen4gksdy5k860fau65c3sw/delegate",
@@ -126,7 +137,9 @@ const logos = {
   iov: {
     image: "/static/images/icons/iov.png",
     name: "Iov",
+    text: "Iov - IOV",
     key: "iov",
+    denom: "IOV",
     delegate:
       "https://big-dipper.iov-mainnet-2.iov.one/validator/starvaloper1jkv2qkpq6cfplx6put7f00wzuyds57fnmtgde0/delegate",
     heightSocket: "wss://ws.iov.forbole.com",
@@ -146,7 +159,9 @@ const logos = {
   akash: {
     image: "/static/images/icons/akash.png",
     name: "Akash",
+    text: "Akash - AKT",
     key: "akash",
+    denom: "AKT",
     delegate:
       "https://akash.bigdipper.live/validator/akashvaloper14kn0kk33szpwus9nh8n87fjel8djx0y0uzn073/delegate",
     heightSocket: "wss://ws.akash.forbole.com",
@@ -166,7 +181,9 @@ const logos = {
   ["band-protocol"]: {
     image: "/static/images/icons/band-protocol.png",
     name: "Band Protocol",
+    text: "Band Protocol - BAND",
     key: "band-protocol",
+    denom: "BAND",
     heightSocket: "wss://ws.band.forbole.com",
     delegate:
       "https://band.bigdipper.live/validator/bandvaloper14kn0kk33szpwus9nh8n87fjel8djx0y0wz502z/delegate",
