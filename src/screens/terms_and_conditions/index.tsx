8dbafb194efef68d64e07a7e687dae68ddc93aed
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-04-23 11:01:00 +0800

    lint test

--- src/screens/terms_and_conditions/index.tsx
+++ src/screens/terms_and_conditions/index.tsx
@@ -166,7 +166,7 @@ const TermsAndConditions = () => {
         of the Company and any of its suppliers under any provision of this
         Terms and Your exclusive remedy for all of the foregoing shall be
         limited to the amount actually paid by You through the Service or 100
-        USD if You haven't purchased anything through the Service.
+        USD if You haven&apos;t purchased anything through the Service.
       </p>
       <p>
         To the maximum extent permitted by applicable law, in no event shall the
@@ -185,7 +185,7 @@ const TermsAndConditions = () => {
         Some states do not allow the exclusion of implied warranties or
         limitation of liability for incidental or consequential damages, which
         means that some of the above limitations may not apply. In these states,
-        each party&rsquo;'s liability will be limited to the greatest extent
+        each party&rsquo;s liability will be limited to the greatest extent
         permitted by law.
       </p>
       <h1>&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h1>
@@ -266,10 +266,10 @@ const TermsAndConditions = () => {
       <h2>Waiver</h2>
       <p>
         Except as provided herein, the failure to exercise a right or to require
-        performance of an obligation under this Terms shall not effect a party's
-        ability to exercise such right or require such performance at any time
-        thereafter nor shall be the waiver of a breach constitute a waiver of
-        any subsequent breach.
+        performance of an obligation under this Terms shall not effect a
+        party&apos;s ability to exercise such right or require such performance
+        at any time thereafter nor shall be the waiver of a breach constitute a
+        waiver of any subsequent breach.
       </p>
       <h1>Translation Interpretation</h1>
       <p>
@@ -281,7 +281,7 @@ const TermsAndConditions = () => {
       <p>
         We reserve the right, at Our sole discretion, to modify or replace these
         Terms at any time. If a revision is material We will make reasonable
-        efforts to provide at least 30 days' notice prior to any new terms
+        efforts to provide at least 30 days&apos; notice prior to any new terms
         taking effect. What constitutes a material change will be determined at
         Our sole discretion.
       </p>
