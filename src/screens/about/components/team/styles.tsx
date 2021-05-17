4c47e4014bbb16c2ca7df4a132878924dbb9e326
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-05-17 11:55:21 +0800

    team section styling

--- src/screens/about/components/team/styles.tsx
+++ src/screens/about/components/team/styles.tsx
@@ -3,6 +3,7 @@ import { MaxWidthContainerCSS as MaxWidthCSS } from "@styles/components";
 import { mixins, media } from "@styles";
 
 export const TeamCSS = styled.div`
+  padding: 0rem 0rem 5rem;
   ${media.bigDesktop`
     ${mixins.flexCenter}
     padding-top: 3rem;
@@ -11,12 +12,14 @@ export const TeamCSS = styled.div`
 `;
 
 export const TeamHeadingContainerCSS = styled.div`
-  margin-bottom: 1rem;
-  padding: 2.5rem 0.5rem 1.5rem;
+  padding: 2.5rem 1.5rem 1.5rem;
   h3 {
     font-weight: 400;
     font-size: 2rem;
     margin-bottom: 0.5rem;
+  }
+  p {
+    line-height: 1.75rem;
   }
 
   ${media.bigDesktop`
@@ -25,6 +28,9 @@ export const TeamHeadingContainerCSS = styled.div`
       font-weight: 200;
       font-size: 2.5rem;
       // padding: 2rem 1rem 0rem;
+    }
+    p {
+      margin: 0;
     }
   `}
 `;
