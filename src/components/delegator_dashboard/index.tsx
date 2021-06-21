fbfd32eee8abddc434c53f55993e7d403503bd39
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-06-22 00:29:43 +0800

    x screenshot

--- src/components/delegator_dashboard/index.tsx
+++ src/components/delegator_dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "i18n";
 import { XScreenshot } from "@icons";
+import Image from "next/image";
 import { DashboardContentCSS } from "./styles";
 
 export const DashboardContent = () => {
@@ -8,12 +9,13 @@ export const DashboardContent = () => {
   return (
     <DashboardContentCSS>
       <div className="desktopWrapper">
-        <div className="image">
-          {/* <XScreenshot /> */}
-          <img
-            src="/static/images/assets/forbole-portal-screenshot.png"
-            alt="Forbole Portal Screenshot"
-          ></img>
+        <div className="image-container">
+          <Image
+            src="/static/images/assets/forbole-x-screenshot.png"
+            alt="Forbole X Screenshot"
+            className="image"
+            layout="fill"
+          />
         </div>
         <img
           src="/static/images/assets/portal-ellipse.png"
